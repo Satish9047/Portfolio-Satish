00f2469a78418570d0ed93ec2bd53d6b2e94aa61
Satish9047 <satishprajapati930@gmail.com>
2024-08-19 18:14:49 +0545

    update project description

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -11,19 +11,21 @@ const Project = () => {
           className="p-4 space-y-4 text-xl text-justify bg-gray-200 rounded-lg shadow-md"
         >
           <h1 className="font-bold ">{project.projectName}</h1>
-          <p className="">{project.description}</p>
-          <button className="text-blue-500 bg-gray-300 rounded-md hover:bg-gray-200">
-            <figure>
-              <a
-                href={project.link}
-                target="_blank"
-                className="flex items-center gap-2 px-4 py-3 font-semibold"
-              >
-                <FaGithub className="w-8 h-8 hover:text-gray-500" />
-                Github
-              </a>
-            </figure>
-          </button>
+          <div className="flex flex-col items-center justify-between gap-4 md:flex-row ">
+            <p className="text-gray-500">{project.description}</p>
+            <button className="text-blue-500 bg-gray-300 rounded-md hover:bg-gray-200">
+              <figure>
+                <a
+                  href={project.link}
+                  target="_blank"
+                  className="flex items-center gap-2 px-4 py-3 font-semibold"
+                >
+                  <FaGithub className="w-8 h-8 hover:text-gray-500" />
+                  Github
+                </a>
+              </figure>
+            </button>
+          </div>
         </div>
       ))}
     </div>
--- src/data/project.ts
+++ src/data/project.ts
@@ -2,18 +2,19 @@ export const projects = [
   {
     projectName: "Meet Home",
     description:
-      "In this projects I have build the frontend with angular and the backend with the typescript, express.js with mongodb as a database. In this project I have implement CRUD operations, authentication, pagination, search functionality, proper validation, error handler, responses type with best code practices.",
+      "Meet home is the fullstack web application where the user can browse the available houses and can book the schedule to visit the house with EState Admin",
     link: "https://github.com/Satish9047/Meet-Home",
   },
   {
     projectName: "EShop (E-commerce app)",
     description:
-      "This project is build with the help of React, Redux toolkit, RTK query, Bootstrap, Node, Express, MongoDB. In this project I have implement Search, CRUD, validation, state management, pagination, authentication, authorization, error handling, and many more with best code practices.",
+      "E-commerce app is the fullstack web application where the user can browse the available products and can add, Update, Delete, Search, Order the product to cart and can buy the product online",
     link: "https://github.com/Satish9047/ecommerce-shop",
   },
   {
     projectName: "Frontend Projects 1",
-    description: "frontend project practice with HTML, CSS, React, Tailwind",
+    description:
+      "List of frontend side projects practice with HTML, CSS, React, Tailwind, SCSS, and JavaScript",
     link: "https://github.com/Satish9047/tailwind-projects",
   },
 ];
